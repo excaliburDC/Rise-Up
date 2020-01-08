78a2b69597fd0f459f69901f4ab05f4a090522c2
DC <43692686+excaliburDC@users.noreply.github.com>
2020-01-08 22:41:38 +0530

    Add files via upload

---
+++ Assets/Scripts/PutBack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PutBack : MonoBehaviour
+{
+    public BirdPool PManager;
+
+    private void OnEnable()
+    {
+        Invoke("PutBackInList", 5f);
+    }
+
+    public void PutBackInList()
+    {
+        if (PManager == null)
+        {
+            PManager = GameObject.Find("PoolManager").GetComponent<BirdPool>();
+            PManager.PutInList(gameObject);
+        }
+        else
+        {
+            PManager.PutInList(gameObject);
+        }
+    }
+
+}
